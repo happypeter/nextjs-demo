0d35c2ea789ff8499d2ca2169a3538d4cf485c71
Billie Zhang <billiecoder@gmail.com>
2017-10-10 17:00:09 +0800

    router

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
+import Link from 'next/link'
 const index = () => (
   <div>
+    <Link as='/v/123' href='/video?id=123'>
+      <a>video page</a>
+    </Link>
     <p>hello world</p>
   </div>
 )
 
 export default index
-
---
+++ pages/video.js
@@ -0,0 +1,8 @@
+const video = (props) => (
+  <div>
+    <div>id: {props.url.query.id}</div>
+    <p>This is video page</p>
+  </div>
+)
+
+export default video
